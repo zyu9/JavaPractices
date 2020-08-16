bc50bc412787dc79f89935772feafd13eef433c7
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-08-16 14:56:44 -0400

    Add files via upload

---
+++ Statics/NoStatic.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+/**
+ * Write a description of class NoStatic here.
+ *
+ * @author (your name)
+ * @version (8/16/2020)
+ */
+public class NoStatic
+{
+    public static void main(String[] args){
+        System.out.println("sqrt " + Math.sqrt(2.0));
+        System.out.println("tan " + Math.tan(60));
+    }
+}
---
+++ Statics/WithStatic.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+import static java.lang.Math.*; 
+/**
+ * Write a description of class WithStatic here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WithStatic
+{
+    public static void main(String[] args){
+        out.println("sqrt " + sqrt(2.0));
+        out.println("tan " + tan(60));
+    }
+}
