706b44af712bfc40a39b2b2b705b056badbd0c0c
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-08-18 14:31:15 -0400

    Add files via upload

---
+++ TestExceptions/TestExceptions.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class TestExceptions here.
+ *
+ * @author (zyu9)
+ * @version (8/18/2020)
+ */
+public class TestExceptions
+{
+    public static void main(String[] args){
+        String test = "no";
+        try{
+            System.out.println("start try");
+            doRisky(test);
+            System.out.println("end try");
+        }catch(ScaryException se){
+            System.out.println("scary exception");
+        }finally{
+            System.out.println("finally");
+        }
+        System.out.println("end of main");
+    }
+    
+    static void doRisky(String test) throws ScaryException{
+        System.out.println("start risky");
+        if("yes".equals(test)){
+            throw new ScaryException();
+        }
+        System.out.println("end risky");
+        return; 
+    }
+}
