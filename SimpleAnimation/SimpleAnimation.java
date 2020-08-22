f8104921864726c101056297c5d8986967e15e63
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-08-22 13:43:36 -0400

    Add files via upload

---
+++ SimpleAnimation/SimpleAnimation.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*; 
+/**
+ * Write a description of class SimpleAnimation here.
+ *
+ * @author (zyu9)
+ * @version (8/22/2020)
+ */
+public class SimpleAnimation
+{
+    int x = 70;
+    int y = 70;
+    
+    public static void main(String[] args){
+        SimpleAnimation gui = new SimpleAnimation();
+        gui.go(); 
+    }
+    
+    public void go(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        MyDrawPanel drawPanel = new MyDrawPanel();
+        
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        
+        for(int i = 0; i < 130; i++){
+            x++;
+            y++;
+            
+            drawPanel.repaint();
+            
+            try{
+                Thread.sleep(50);                
+            }catch(Exception ex){}
+        }
+    }//close go() method
+    
+    class MyDrawPanel extends JPanel{
+        public void paintComponent(Graphics g){
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(),this.getHeight());
+            
+            g.setColor(Color.green);
+            g.fillOval(x,y,40,40);
+        }
+    }//close inner class
+}//close outer class
