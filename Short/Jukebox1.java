1068259c14b2b50a7774caa073f49d96589692fd
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-09-06 15:07:57 -0400

    Add files via upload

---
+++ Short/Jukebox1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*; 
+/**
+ * Write a description of class Jukebox1 here.
+ *
+ * @author (zyu9)
+ * @version (9/6/2020)
+ */
+public class Jukebox1
+{
+   ArrayList<String> songList = new ArrayList<String>();
+   
+   public static void main(String[] args){
+       new Jukebox1().go(); 
+   }
+   
+   public void go(){
+       getSongs();
+       System.out.println(songList);
+       Collections.sort(songList);
+       System.out.println(songList);
+   }
+   
+   void getSongs(){
+       try{
+           File file = new File("SongList.txt");
+           BufferedReader reader = new BufferedReader(new FileReader(file));
+           String line = null;
+           while((line = reader.readLine()) != null){
+               addSong(line);
+           }
+       }catch(Exception ex){
+           ex.printStackTrace();
+       }
+   }
+   
+   void addSong(String lineToParse){
+       String[] tokens = lineToParse.split("/");
+       songList.add(tokens[0]);
+   }
+}
