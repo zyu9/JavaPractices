111c314fcf99f9219f8e584e27e0420d94a1c58a
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-09-17 17:42:49 -0400

    Add files via upload

---
+++ Errors/LexicalError.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class LexicalError here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LexicalError
+{
+    public void Case(){
+        //case n = 1; 
+        //foo a = 2; 
+        //int 8var;
+    }
+}
---
+++ Errors/SyntaxError.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class SyntaxError here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SyntaxError
+{
+    public static void main(Strig[] args){
+        
+    }
+}
