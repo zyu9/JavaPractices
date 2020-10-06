ffee625536c295aa2a38be003b0faa14b3122306
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-10-06 16:49:32 -0400

    Add files via upload

--- TestGenerics/TestGenerics2.java
+++ TestGenerics/TestGenerics2.java
@@ -20,7 +20,7 @@ public class TestGenerics2
         takeAnimals(animals);
     }
     
-    public void takeAnimals(ArrayList<Animal> animals){
+    public void takeAnimals(ArrayList<? extends Animal> animals){
         for(Animal a: animals){
             a.eat(); 
         }
